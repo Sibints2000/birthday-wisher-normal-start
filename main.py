65b373cdf9b146151cc28d3be38ffad95f203444
Sibints2000 <sibints2000@gmail.com>
2021-08-27 17:19:34 +0530

    Nineteenth Commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import pandas
+import random
 
 today = datetime.now()
 today_tuple = (today.month, today.day)
@@ -7,7 +8,7 @@ today_tuple = (today.month, today.day)
 data = pandas.read_csv("birthdays.csv")
 birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 if today_tuple in birthdays_dict:
-
+    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
 
 # 3. If there is a match, pick a random letter (letter_1.txt/letter_2.txt/letter_3.txt) from letter_templates and replace the [NAME] with the person's actual name from birthdays.csv
 # HINT 1: Think about the relative file path to open each letter. 
