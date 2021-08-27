c0b9ae503257b03f4b37eb31bad2bdf21f569beb
Sibints2000 <sibints2000@gmail.com>
2021-08-27 17:15:43 +0530

    Thirteenth Commit

--- main.py
+++ main.py
@@ -6,20 +6,11 @@ today = datetime.now()
 today_tuple = (today.month, today.day)
 
 data = pandas.read_csv("birthdays.csv")
-
-# HINT 3: Use dictionary comprehension to create a dictionary from birthday.csv that is formated like this:
 birthdays_dict = {
     (birthday_month, birthday_day): data_row
 }
-#Dictionary comprehension template for pandas DataFrame looks like this:
-birthdays_dict = {(data_row["month"], data_row["day"]): new_value for (index, data_row) in data.iterrows()}
-#e.g. if the birthdays.csv looked like this:
-# name,email,year,month,day
-# Angela,angela@email.com,1995,12,24
-#Then the birthdays_dict should look like this:
-# birthdays_dict = {
-#     (12, 24): Angela,angela@email.com,1995,12,24
-# }
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
 
 #HINT 4: Then you could compare and see if today's month/day tuple matches one of the keys in birthday_dict like this:
 # if (today_month, today_day) in birthdays_dict:
